77b9db551ee8693a78bb04328b64a335ad582d90
Boris Kaul <localvoid@gmail.com>
2015-03-28 17:01:58 +0600

    Update

--- web/src/board.g.dart
+++ web/src/board.g.dart
@@ -1,5 +1,5 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// 2015-03-22T11:19:46.696Z
+// 2015-03-28T10:51:52.321Z
 
 part of app.src.ui.component.board;
 
@@ -8,17 +8,16 @@ part of app.src.ui.component.board;
 // Target: class Cell
 // **************************************************************************
 
-Cell createCell([s.Cell data, Component parent]) {
-  final r = new Cell()
+Cell createCell([s.Cell data, List<VNode> children, Component parent]) {
+  return new Cell()
     ..parent = parent
-    ..data = data;
-  r.init();
-  return r;
+    ..data = data
+    ..children = children;
 }
 VNode vCell({s.Cell data, Object key, String type, Map<String, String> attrs,
     Map<String, String> style, List<String> classes,
     List<VNode> children}) => new VNode.component(createCell,
-    flags: VNode.componentFlag | VNode.dirtyCheckFlag,
+    flags: VNode.componentFlag,
     key: key,
     data: data,
     type: type,
@@ -32,17 +31,16 @@ VNode vCell({s.Cell data, Object key, String type, Map<String, String> attrs,
 // Target: class Board
 // **************************************************************************
 
-Board createBoard([dynamic data, Component parent]) {
-  final r = new Board()
+Board createBoard([dynamic data, List<VNode> children, Component parent]) {
+  return new Board()
     ..parent = parent
-    ..data = data;
-  r.init();
-  return r;
+    ..data = data
+    ..children = children;
 }
 VNode vBoard({dynamic data, Object key, String type, Map<String, String> attrs,
     Map<String, String> style, List<String> classes,
     List<VNode> children}) => new VNode.component(createBoard,
-    flags: VNode.componentFlag | VNode.dirtyCheckFlag,
+    flags: VNode.componentFlag,
     key: key,
     data: data,
     type: type,
